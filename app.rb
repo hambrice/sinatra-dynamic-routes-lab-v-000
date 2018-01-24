821aa8194ba2293ac8b8f09f7a46ea470b2acfa4
Brad Hambrice <hambrice@uab.edu>
2018-01-24 21:30:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name' do
+    params[:name].reverse!
+  end
 
+  get '/square/:number' do
+     number = params[:number].to_i
+    answer = number * number
+    answer.to_s
+  end
+
+  get '/say/:number/:phrase' do
+    count = params[:number].to_i
+    array = []
+    count.times do
+      array << params[:phrase]
+    end
+    array.join(" ")
+  end
 end
